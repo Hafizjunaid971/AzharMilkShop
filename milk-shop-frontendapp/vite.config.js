79231aec622242723f1b682c12c55e8e39b030bc
unknown <hafizjunaid971@gmail.com>
2026-02-08 01:05:00 +0500

    url set

--- milk-shop-frontendapp/vite.config.js
+++ milk-shop-frontendapp/vite.config.js
@@ -1,10 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     historyApiFallback: true, // ye chahiye SPA routing ke liye
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // ye chahiye SPA routing ke liye
-  },
+  base: './',  // <- important for Vercel SPA
 })
-
